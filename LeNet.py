4425ca324e6635d02e781837a4d75af593ee6a57
Jinay Patel <jinay1991@gmail.com>
2017-09-15 19:11:27 +0530

    Improved Accuracy by tweaking few of the parameters

--- LeNet.py
+++ LeNet.py
@@ -111,7 +111,7 @@ def main(unused_argv):
                                                         num_epochs=None,
                                                         shuffle=True)
     mnist_classifier.train(input_fn=train_input_fn,
-                           steps=2000,
+                           steps=20000,
                            hooks=[logging_hook])
 
     # Evaluate Model
